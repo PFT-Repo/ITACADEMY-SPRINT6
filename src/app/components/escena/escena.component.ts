12f6d3620ea023f2ff77e223c6f5027ae8ecaeef
TurpialCode <84598876+PFT-Repo@users.noreply.github.com>
2022-11-15 12:47:59 +0100

    final commit #welcomeAngular

--- src/app/components/escena/escena.component.ts
+++ src/app/components/escena/escena.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {p} from './p';
-import { titles } from './mock_p';
+import { Component, OnInit, Input } from '@angular/core';
+import { p } from '../home/home.component';
 
 @Component({
   selector: 'app-escena',
@@ -9,10 +8,40 @@ import { titles } from './mock_p';
 })
 export class EscenaComponent implements OnInit {
 
-  Data = titles;
-  constructor() { }
 
-  ngOnInit(): void {
+  @Input() titols: p[] = [];
+  currentSentence: p | undefined;
+  i : number = 0;
+  bg = document.getElementById("body");
+  constructor() {
   }
+  next() {
+    this.i +=1;
+    if(this.i>3){this.i = 3}
+    this.changback();
+  }
+  prev() {
+    this.i-=1
+    if(this.i < 0){this.i=0}
+    this.changback();
+  }
+  ngOnInit(): void {
+    this.currentSentence = this.titols[this.i]
+      let bu =document.getElementById('body');
+      if (bu){
+        let y = <HTMLImageElement> bu;
+        y.src= "assets/img/"+this.titols[this.i].img;
+        bu.style.backgroundImage ="url(assets/img/"+this.titols[this.i].img+")";
+      }
+      
+  }
+  changback(){
+    let bu =document.getElementById('body');
+      if (bu){
+        let y = <HTMLImageElement> bu;
+        y.src= "assets/img/"+this.titols[this.i].img;
+        bu.style.backgroundImage ="url(assets/img/"+this.titols[this.i].img+")";
+  }
+}
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  firstTime = true;
   constructor() { }
 
   ngOnInit(): void {
   }
+  changeFirst(){
+    this.firstTime= false;
+  }
+  
+  titles: p[] = [
+    {
+      title: "Nuestro héroe estaba flotando por el espacio sideral cuando a lo lejos divisó una nave espacial",
+      img:"1.jpg"
+    },
+    {
+      title: "Sentía curiosidad por el interior de la nave y se puso a inspeccionarla. Llegó a una sala con dos puertas.",
+      img:"2.jpg"
+    },
+    {
+      title: "El héroe decidió atravesar la puerta que le llevaba a casa",
+      img:"3.jpg"
+    },
+    {
+      title: "Mientras tanto, otras heroes no tuvieron tanta suerte en su elección...",
+      img:"4.jpg"
+    }
+  ]
 
+}
+
+export interface p {
+    id?: number;
+    title?:string;
+    img?:string;
 }
